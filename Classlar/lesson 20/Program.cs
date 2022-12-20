9c56ed869c74310afa61af7195f4a3a5063ebfc0
daelvinci <elvinab@code.edu.az>
2022-12-20 18:49:25 +0400

    20.12.2022

---
+++ Classlar/lesson 20/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace lesson_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        //static int[] WithInt(int[] arr, int num)
+        //{
+        //    int[] newArr = new int[arr.Length + 1];
+
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        newArr[i] = arr[i];
+
+        //    }
+        //    newArr[newArr.Length - 1] = num;
+
+        //    return newArr;
+        //}
+    }
+}
