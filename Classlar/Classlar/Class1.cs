f2ea72500ab26f45a25c5b9acfad14f078e02f97
daelvinci <elvinab@code.edu.az>
2022-12-20 10:23:02 +0400

    Classlar

---
+++ Classlar/Classlar/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classlar
+{
+    internal class Cars
+    {
+        public string Brand;
+        public string Model;
+        public int Year;
+        public double Price;
+        public double Millage;
+        public double GlobalMillage;
+
+        public void Drive (double km)
+        {
+            Millage += km;
+            GlobalMillage += km;
+           
+        }
+
+        public void ResetMillage()
+        {
+             Millage = 0;
+
+        }
+
+
+       
+    }
+}
---
+++ Classlar/Classlar/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Classlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cars car = new Cars();
+            car.Brand = "";
+            car.Model = "";
+            car.Price = 2600;
+            car.Year=2020;
+
+
+            Cars car2 = new Cars();
+            car2.Brand = "ada";
+
+            var masin = new
+            {
+                brand = "mercedes",
+                year = 2022,
+                price= 2400
+
+            };
+
+            var huseyn = new 
+            {
+               agil= "low level",
+               xasiyyet="neblimala",
+               dozumluluk= "high"
+
+            };
+
+            var car1 = new 
+            {
+                brand="Bmw",
+                Year="2020"
+            
+            };
+
+
+
+        }
+        static double Average(Cars[] carsArray)
+        {
+          
+            double sum = 0;
+            
+            for(int i=0; i<carsArray.Length; i++)
+            {
+                sum +=carsArray[i].Price;
+               
+
+            }
+            return sum / carsArray.Length;
+
+
+        }
+    }
+}
