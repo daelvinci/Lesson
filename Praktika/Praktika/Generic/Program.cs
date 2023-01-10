628c4a9b82b552bc0ad53812060771150b3c4a78
daelvinci <elvinab@code.edu.az>
2023-01-10 13:07:22 +0400

    dad

--- Praktika/Praktika/Generic/Program.cs
+++ Praktika/Praktika/Generic/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Xml.Linq;
 
 namespace Generic
 {
@@ -7,6 +9,23 @@ namespace Generic
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            ArrayList list = new ArrayList()
+            {
+             "asadsad",
+             "dakwdnad",
+             2e2,
+
+            };
+            list.Add("salam");
+            list.Add(123);
+
+            Queue sira = new Queue();
+            sira.Enqueue('d');
+            sira.Enqueue("d");
+            sira.Dequeue();
+            sira.Peek();
+
+
         }
     }
 }
